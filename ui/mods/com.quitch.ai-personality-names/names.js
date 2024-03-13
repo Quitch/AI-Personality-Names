429784a8b8bb9af3148eccc89ef2501e9bf3ee07
Quitch <Quitch@users.noreply.github.com>
2024-03-13 10:28:50 +0000

    Add error handling

--- ui/mods/com.quitch.ai-personality-names/names.js
+++ ui/mods/com.quitch.ai-personality-names/names.js
@@ -7,15 +7,20 @@ function aiPersonalityNames() {
 
   aiPersonalityNamesLoaded = true;
 
-  var replaceHtml = function (htmlClass, file) {
-    var path = "coui://ui/mods/com.quitch.ai-personality-names/";
-    $(htmlClass).replaceWith(loadHtml(path + file));
-  };
+  try {
+    var replaceHtml = function (htmlClass, file) {
+      var path = "coui://ui/mods/com.quitch.ai-personality-names/";
+      $(htmlClass).replaceWith(loadHtml(path + file));
+    };
 
-  replaceHtml("div.div_player_name.truncate", "playing_names.html");
-  replaceHtml(
-    "td.div_name_header.div_spectator_player_info",
-    "spectating_names.html"
-  );
+    replaceHtml("div.div_player_name.truncate", "playing_names.html");
+    replaceHtml(
+      "td.div_name_header.div_spectator_player_info",
+      "spectating_names.html"
+    );
+  } catch (e) {
+    console.error(e);
+    console.error(JSON.stringify(e));
+  }
 }
 aiPersonalityNames();
