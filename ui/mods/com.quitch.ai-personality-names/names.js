3c537577ccbdbe8ec7839f36334c00be4048630f
Quitch <Quitch@users.noreply.github.com>
2026-08-11 10:49:38 +0100

    Replace const with var due to Chrome 40 bugs

--- ui/mods/com.quitch.ai-personality-names/names.js
+++ ui/mods/com.quitch.ai-personality-names/names.js
@@ -8,8 +8,8 @@ function aiPersonalityNames() {
   aiPersonalityNamesLoaded = true;
 
   try {
-    const replaceHtml = function (htmlClass, file) {
-      const path = "coui://ui/mods/com.quitch.ai-personality-names/";
+    var replaceHtml = function (htmlClass, file) {
+      var path = "coui://ui/mods/com.quitch.ai-personality-names/";
       $(htmlClass).replaceWith(loadHtml(path + file));
     };
 
